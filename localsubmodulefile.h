b5de12abb9e6f0aecff688dd00164df80fa3ea92
Rumen Belev <rumen.belev@egt.com>
2024-11-22 16:38:00 +0200

    Sub change

--- localsubmodulefile.h
+++ localsubmodulefile.h
@@ -1,4 +1,2 @@
-
-Same file name but changes made in the original submodule repo
-Added a second line
+We're using git submodules to manage a couple of large projects that have dependencies on many other libraries we've developed. Each library is a separate repo brought into the dependent project as a submodule. During development, we often want to just go grab the latest version of every dependent submodule.
 
